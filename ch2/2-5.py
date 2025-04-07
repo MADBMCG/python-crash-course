9ff901c3e3a6173765ae69118aeba1dcbe205ca1
MADBMCG <maddie.b.mcguire@gmail.com>
2025-04-07 16:40:20 -0700

    More ch2 exercises

---
+++ ch2/2-5.py
@@ -0,0 +1,3 @@
+"""Exercise 2-5: print a quote"""
+quote = 'My mom once said, "only boring people get bored."'
+print(quote)
---
+++ ch2/2-6.py
@@ -0,0 +1,6 @@
+"""Exercise 2-6: Repeat 2-5, but this time with the person who said the quote as a variable"""
+
+person = "My mom"
+quote = "only boring people get bored"
+
+print(f'{person} once said, "{quote}."')
