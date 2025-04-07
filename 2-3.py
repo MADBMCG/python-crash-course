673e05af46dbb119e55e5eb02823ee4dc16fb52f
MADBMCG <maddie.b.mcguire@gmail.com>
2025-04-07 16:32:27 -0700

    2-4

--- 2-3.py
+++ 2-3.py
@@ -1,4 +1,4 @@
-"""Excercise 2-3"""
+"""Excercise 2-3: Use a variable to represent a person's name, and print a message to that person."""
 
 name = "Maddie"
 print(f"Hey {name}, good job doing your Python excercises today!")
---
+++ 2-4.py
@@ -0,0 +1,5 @@
+"""Exercise 2-4: Use a variable to represent a person's name, then print the name in lowercase, uppercase, and title case."""
+name = "Maddie"
+print(name.lower())
+print(name.upper())
+print(name.title())
